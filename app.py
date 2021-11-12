729c16093a8130f32c744a4238289de4ed3ce32e
KavenChen <swtyuo56322p@gmail.com>
2021-11-12 13:31:12 +0800

    Add button to take photo

--- app.py
+++ app.py
@@ -6,10 +6,14 @@ from PIL import Image
 import cv2
 import simplejpeg as sjpg
 import requests
+import requests.packages.urllib3
+requests.packages.urllib3.disable_warnings()
 import random
 import os
 import io
 import time
+import logging
+import numpy as np
 # 
 from meter import SubMeter
 from plotting import draw, names
@@ -32,14 +36,14 @@ RTC_CONFIGURATION = RTCConfiguration(
 )
 
 # address = 'https://140.117.75.46:2087/detect_img_jpg'
-# address = 'https://192.168.1.7:2087/detect_img_jpg'
+address = 'https://192.168.1.7:2087/detect_img_jpg'
 
 # simple YOLOv5
 # address = 'http://192.168.1.6:3100/single_jpg'
 # address = 'http://140.117.75.46:3100/single_jpg'
 # address = 'https://140.117.75.46:3100/single_jpg'
 # address = 'https://datasci.mis.nsysu.edu.tw:3100/single_jpg'
-address = 'http://localhost:3100/single_jpg'
+# address = 'https://localhost:3100/single_jpg'
 
 class FlaskFaceDetection:
     def __init__(self):
@@ -47,6 +51,7 @@ class FlaskFaceDetection:
         self.session = requests.Session()
         self.session.trust_env = False
         self.sm = SubMeter()
+        self.response = SubMeter()
         self.color_set = {}
         self.result_queue = queue.Queue()
 
@@ -54,7 +59,6 @@ class FlaskFaceDetection:
         self.result_queue = queue.Queue()
         self.result_queue.put(frame)
         ori_img = frame.to_ndarray(format='bgr24') # type is numpy
-
         # shape is (h, w, 3)
         
         print(ori_img.shape)
@@ -62,66 +66,83 @@ class FlaskFaceDetection:
         # ----- setting width and height skiped -----
 
         if self.remote_send:
-            print('send')
+#             print('send')
             self.remote_send = False
+            
+            t1 = time.time()
 
             # ----- compress to jpeg -----
-            img = sjpg.encode_jpeg(ori_img) #bytes
+            img = sjpg.encode_jpeg(ori_img)
             # with open('temp.jpg', 'wb') as f:
             #     f.write(img)
 
             # ----- request backend -----
-            res = self.session.post( # -> server_flask
+            res = self.session.post(
                 address,
                 files={
-                    'picture': ('upload.jpg', io.BytesIO(img)) # Camouflaged file
+                    'picture': ('upload.jpg', io.BytesIO(img))
                 },
                 headers={'Connection': 'close'},
-                verify=False, # For CSIE SSL issue
+                verify=False,
             )
             
-            # # ----- draw -----
-            # res = res.json()
-            # print(res)
-            # self.sm.times_update(res['time'])
-            # self.sm.plot()
-
-            # is_identified = [r['name'] for r in res['registered']]
-
-            # out_img = ori_img
-
-            # for n in is_identified:
-            #     if n not in self.color_set:
-            #         self.color_set[n] = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
-
-            # for i, face in enumerate(res['faces']):
-            #     p1 = tuple(face[0])
-            #     p2 = tuple(face[1])
-            #     c = self.color_set[is_identified[i]]
-            #     img = cv2.rectangle(out_img, p1, p2, c, 10)
-
-            # for reg in res['registered']:
-            #     text = '{:.02f} {}'.format(reg['confidence'], reg['name'])
-            #     f_idx = reg['face_id']
-            #     loc = res['faces'][f_idx][0]
-            #     loc = (loc[0], loc[1] - 10)
-            #     c = self.color_set[reg['name']]
-            #     cv2.putText(out_img, text, loc, cv2.FONT_HERSHEY_DUPLEX, 1, c, 2, cv2.LINE_AA)
-
-
-            # draw for .6 YOLOv5
+            # ----- draw -----
             res = res.json()
-            print('res', res)
+            print(res)
+            self.sm.times_update(res['time'])
+            self.sm.plot()
 
-            class Res:
-                pass
-            res_cls = Res()
-            res_cls.names = names
-            for k, v in res.items():
-                setattr(res_cls, k, v)
+            is_identified = [r['name'] for r in res['registered']]
 
             out_img = ori_img
-            out_img = draw(res_cls, [ori_img])[0]
+
+            for n in is_identified:
+                if n not in self.color_set:
+                    self.color_set[n] = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+
+            for i, face in enumerate(res['faces']):
+                p1 = tuple(face[0])
+                p2 = tuple(face[1])
+                c = self.color_set[is_identified[i]]
+                img = cv2.rectangle(out_img, p1, p2, c, 10)
+
+            for reg in res['registered']:
+                text = '{:.02f} {}'.format(reg['confidence'], reg['name'])
+                f_idx = reg['face_id']
+                loc = res['faces'][f_idx][0]
+                loc = (loc[0], loc[1] - 10)
+                c = self.color_set[reg['name']]
+                cv2.putText(out_img, text, loc, cv2.FONT_HERSHEY_DUPLEX, 1, c, 2, cv2.LINE_AA)
+
+
+            # ----- draw for .6 YOLOv5 -----
+            # res = res.json()
+            # print('res', res)
+
+            # class Res:
+            #     names = names
+            # res_cls = Res()
+            # for k, v in res.items():
+            #     setattr(res_cls, k, v)
+            
+            # self.sm.update(res['encode_time'], res['modeling_time'], 1e-10, 1e-10)
+            # self.sm.plot()
+            # if len(self.sm.total_list) == 30:
+            #     print('fps', 1 / (sum(self.sm.total_list) / len(self.sm.total_list)))
+            #     logging.warning(f'avg {np.mean(self.sm.total_list)}')
+            #     logging.warning(f'fps {1 / np.mean(self.sm.total_list)}')
+            
+            # t1 = time.time() - t1
+            # self.response.update(t1, 1e-10, 1e-10, 1e-10)
+            # self.response.plot()
+            # if len(self.response.total_list) == 30:
+            #     print('fps', 1 / (sum(self.response.total_list) / len(self.response.total_list)))
+            #     logging.warning(f'avg {np.mean(self.response.total_list)}')
+            #     logging.warning(f'fps {1 / np.mean(self.response.total_list)}')
+
+            # out_img = ori_img
+            # out_img = draw(res_cls, [ori_img])[0]
+
             
             self.remote_send = True
 
@@ -156,6 +177,7 @@ st_show = st.empty()
 #     st_show.image(video_frame)
 
 if st.button('Take picture'):
+    print('Take picture')
     # if ctx.state.playing:
     #     labels_placeholder = st.empty()
     # while True:
@@ -164,11 +186,14 @@ if st.button('Take picture'):
             video_frame = ctx.video_processor.result_queue.get(
                 timeout=1.0
             )
+            video_frame = video_frame.to_ndarray(format='rgb24')
+            st_show.image(video_frame)
+            ctx._set_worker(None)
+            print("ctx._set_worker(None)")
         except queue.Empty:
-            print('break1')
+            print('queue.Empty')
             # break
-        video_frame = video_frame.to_ndarray(format='rgb24')
-        st_show.image(video_frame)
     else:
-        print('break2')
+        print('not ctx.video_processor')
         # break
+
